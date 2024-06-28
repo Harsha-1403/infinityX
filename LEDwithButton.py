243783918d38323046a4a952bae4b052cc3227c4
Harsha-1403 <141326052+Harsha-1403@users.noreply.github.com>
2024-06-28 15:34:31 +0530

    Add files via upload

--- LEDwithButton.py
+++ LEDwithButton.py
@@ -1,15 +1,31 @@
 from gpiozero import LED, Button
 from time import sleep
 
-led = LED(24)
-lock_sensor_handle = Button(10)
-lock_sensor_inflow = Button(9)
+green = LED(7)
+red = LED(8)
+blue = LED(11)
+lock_sensor_handle = Button(26)
+lock_sensor_inflow = Button(20)
+
+#yellowWire --> down lock
+#orangeWire --> upper lock
+
 
 while True:
-    if lock_sensor_handle.is_pressed or lock_sensor_inflow.is_pressed:
-        led.on()
-        sleep(0.35)
-        led.off()
-        sleep(0.35)
-        led.close()
-        break
+    if lock_sensor_handle.is_pressed:
+        red.on()
+        green.off()
+        blue.off()
+        sleep(3)
+    if lock_sensor_inflow.is_pressed:
+        red.off()
+        green.on()
+        blue.off()
+        sleep(3)
+    if lock_sensor_inflow.is_pressed and lock_sensor_handle.is_pressed:
+        red.off()
+        green.off()
+        blue.on()
+        sleep(3)
+  
+
